cae273db29a9b80868e66a9bcf8342aba65b0929
jeffc2024 <jeffc@unc.edu>
2024-09-14 10:50:01 -0400

    Progress on ex02

---
+++ exercises/ex02_chardle.py
@@ -0,0 +1,56 @@
+"""EX02 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730754025"
+
+
+def input_word() -> str:
+    userWord = input("Enter a 5-character word:")
+    if len(userWord) == 5:
+        return userWord
+    else:
+        print("Error: Word must contain 5 characters.")
+        exit()
+
+
+def input_letter() -> str:
+    userLetter = input("Enter a single character:")
+    if len(userLetter) == 1:
+        return userLetter
+    else:
+        print("Error: Character must be a single character.")
+        exit()
+
+
+def contains_char(word: str, letter: str) -> None:
+    """main part of comparing letters to words, use loops normally"""
+    count = 0
+    print("Searching for " + letter + " in " + word)
+    if word[0] == letter:
+        print(letter + " found at index 1")
+        count += 1
+    if word[1] == letter:
+        print(letter + " found at index 2")
+        count += 1
+    if word[2] == letter:
+        print(letter + " found at index 3")
+        count += 1
+    if word[3] == letter:
+        print(letter + " found at index 4")
+        count += 1
+    if word[4] == letter:
+        print(letter + " found at index 5")
+        count += 1
+    if count == 0:
+        print("No instances of " + letter + " found in " + word)
+    else:
+        """final else statement where no instances found"""
+        print(str(count) + " instances of " + letter + " found in " + word)
+
+
+def main() -> None:
+    """main function, set variables and return types equal here"""
+    contains_char(word=input_word(), letter=input_letter())
+
+
+if __name__ == "__main__":
+    main()
